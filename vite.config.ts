d23c9df487ad83c1d77d65dc50e7246733f8ed42
mogesT.23 <46963563+mogest23@users.noreply.github.com>
2025-05-21 16:48:07 -0500

    Add PORT environment variable and configure Vite server and preview settings to use port 5173

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 5173,
+    host: true,
+    strictPort: true,
+    allowedHosts: [
+      'wap-project-react.onrender.com',
+      'localhost',
+      '127.0.0.1'
+    ]
+  },
+  server: {
+    port: 5173,
+    host: true,
+    strictPort: true
+  }
 })
