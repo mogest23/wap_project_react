9e249667f4a04fc29ab84e73d917d08ddb99e955
mogesT.23 <46963563+mogest23@users.noreply.github.com>
2025-05-23 00:10:39 -0500

    Update footer year from 2024 to 2025

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="bg-green-800 text-gray-300 py-2 text-right text-sm pr-4">
-            <p>WAP Project, May 2024</p>
+            <p>WAP Project, May 2025</p>
         </footer>
     );
 };
